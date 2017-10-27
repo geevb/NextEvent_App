c6f9037c7e0ac96bd98561526229846c31c759d1
Getúlio Benincá <getulio.beninca@nexxera.com>
2017-10-27 15:59:37 -0200

    First commit

---
+++ app/src/main/java/com/example/t3_dsoo2/TelaListaFestasActivity.java
@@ -0,0 +1,16 @@
+package com.example.t3_dsoo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaListaFestasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_lista_festas);
+    }
+
+
+    
+}
